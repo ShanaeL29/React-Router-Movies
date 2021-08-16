0884415380e6e3024a7008d2d89b6c546ad2ab0a
Shanae <sleslie18@gmail.com>
2021-08-16 19:37:36 -0400

    Completed Project

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,28 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+// import MovieCard from "./MovieCard";
+
+import {
+  useParams,
+  // useRouteMatch,
+  // Route,
+  // Switch,
+  // NavLink,
+} from "react-router-dom";
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
+  // const { movies } = props;
 
-  let id = 1;
+  // let id = 1; we changed this line to the line below
+  const { movieId } = useParams();
+  // const { path, url } = useRouteMatch();
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
 
   useEffect(() => {
     axios
-      .get(`http://localhost:5000/api/movies/${id}`) // Study this endpoint with Postman
-      .then(response => {
+      .get(`http://localhost:5000/api/movies/${movieId}`) // Study this endpoint with Postman
+      .then((response) => {
         // Study this response with a breakpoint or log statements
+        console.log(response);
         // and set the response data as the 'movie' slice of state
+        setMovie(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
     // This effect should run every time time
     // the `id` changes... How could we do this?
-  }, []);
+  }, [movieId]);
 
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = evt => { }
+
+  // movies.find((movie) => (movie.id === movieId) === parseInt(movieId)); //id is already being plugged in as a string on line 23 so we don't need this here
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -42,7 +58,7 @@ export default function Movie(props) {
         </div>
         <h3>Actors</h3>
 
-        {stars.map(star => (
+        {stars.map((star) => (
           <div key={star} className="movie-star">
             {star}
           </div>
